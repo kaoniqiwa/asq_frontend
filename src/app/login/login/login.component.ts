29118cd456a808c27ea61f6235d4b9f6939784e2
panminxiang <1061886912@qq.com>
2022-08-09 23:10:37 +0800

    pmx

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 import { EnterPriseLoginModel, LoginModel } from 'src/app/view-model/login.model';
 @Component({
@@ -23,7 +24,9 @@ export class LoginComponent implements OnInit {
   @Output()
   validate = new EventEmitter();
 
-  constructor(private _fb: FormBuilder,) { }
+  constructor(private _title: Title, private _fb: FormBuilder,) {
+    this._title.setTitle('用户登录')
+  }
 
   ngOnInit(): void {
 
--- src/app/neoballoon-system/components/neoballoon-component.module.ts
+++ src/app/neoballoon-system/components/neoballoon-component.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HowellModule } from 'src/app/common/howell.module';
+import { MaterialModule } from 'src/app/material.module';
 import { AccountInfoComponent } from './account-info/account-info.component';
 import { AccountComponent } from './account/account.component';
 import { BabyAddComponent } from './baby-add/baby-add.component';
 import { BabyAgeComponent } from './baby-age/baby-age.component';
 import { BabyGameComponent } from './baby-game/baby-game.component';
+import { BabyInfoOperateComponent } from './baby-info-operate/baby-info-operate.component';
 import { BabyInfoComponent } from './baby-info/baby-info.component';
 import { BabySettingComponent } from './baby-setting/baby-setting.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 import { NeoballoonManageComponent } from './neoballoon-manage/neoballoon-manage.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
-import { RouterModule } from '@angular/router';
-import { HowellModule } from 'src/app/common/howell.module';
-import { MaterialModule } from 'src/app/material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BabyInfoOperateComponent } from './baby-info-operate/baby-info-operate.component';
 
 
 
